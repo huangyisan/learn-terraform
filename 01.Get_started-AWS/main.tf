5d9193651c914fcf897d86a8462d7118459e7900
Yisan Huang <anonymousyisan@gmail.com>
2023-06-28 15:53:32 +0800

    update: tf

--- 01.Get_started-AWS/main.tf
+++ 01.Get_started-AWS/main.tf
@@ -14,12 +14,39 @@ provider "aws" {
   region  = "ap-southeast-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-073998ba87e205747"
-  instance_type = "t2.micro"
+# resource "aws_instance" "app_server" {
+#   ami           = "ami-073998ba87e205747"
+#   instance_type = "t2.micro"
+
+#   tags = {
+#     "Name"    = var.instance_name
+#     "Project" = "ubuntu"
+#     "monitor" = "true"
+#   }
+# }
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "production"
+  }
+}
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 
   tags = {
-    "Name"    = var.instance_name
-    "Project" = "test"
+    Name = "main-subnet"
+  }
+}
+
+resource "aws_subnet" "main_subnet02" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "main-subnet02"
   }
 }
